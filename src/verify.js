69f4e33b63c05425a8c0d7d7dedde69e914b04e4
rahuljha661 <rahuljha12527@gmail.com>
2020-09-12 00:50:55 +0530

    home page complete now working on verify page

---
+++ src/verify.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class verify extends Component {
+    render() {
+        return (
+            <div class="containers">
+                <div><h2>Please verify Mobile number</h2></div>
+                 
+                 <div>An Otp is sent to +918750037661</div>
+                         
+            </div>
+        );
+    }
+}
+
+export default verify;
